5dfd5693981ac342d92ef148a097c8836175cbe2
John Benson <jfbenson@umich.edu>
2011-08-23 15:56:02 -0400

    Added a new sample application for calculating SalesTax. Uses CSS, JavaScript, and XHTML.

---
+++ Chapter1/SalesTax/sales_tax.js
@@ -0,0 +1,28 @@
+var $ = function (id) {
+	return document.getElementById(id);
+}
+
+var calculate_click = function() {
+	var subtotal = parseFloat( $("subtotal").value );
+	var taxRate = parseFloat( $("taxRate").value );
+
+	$("salesTax").value = "";
+	$("total").value = "";
+
+	if( isNaN(subtotal) || subtotal < 0 ) {
+		alert("Subtotal must be a number that is zero or more!");
+	} else if( isNaN(taxRate) || taxRate < 0 ) {
+		alert("Tax Rate must be a number that is zero or more!");
+	} else {
+		var salesTax = subtotal * (taxRate / 100);
+		salesTax = parseFloat( salesTax.toFixed(2) );
+		var total = subtotal + salesTax;
+		$("salesTax").value = salesTax;
+		$("total").value = total.toFixed(2);
+	}
+}
+
+window.onload = function () {
+	$("calculate").onclick = calculate_click;
+	$("subtotal").focus;
+}
